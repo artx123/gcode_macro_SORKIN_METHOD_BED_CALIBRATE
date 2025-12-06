279a838e983088a989a35891d9e4a524f5118ec5
art1234567891 <106118176+artx123@users.noreply.github.com>
2025-12-06 05:30:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 source /opt/config/mod/.shell/0.sh
 
-INC="[include plugins/gcode_macro-SORKIN_METHOD_BED_CALIBRATE/new_macro_bed_call.cfg]"
+INC="[include plugins/gcode_macro_SORKIN_METHOD_BED_CALIBRATE/new_macro_bed_call.cfg]"
 FILE="/opt/config/mod_data/plugins.cfg"
 PLUGIN_DIR="/opt/config/mod_data/plugins/gcode_macro_SORKIN_METHOD_BED_CALIBRATE"
 REPO_URL="https://github.com/artx123/gcode_macro_SORKIN_METHOD_BED_CALIBRATE.git"
