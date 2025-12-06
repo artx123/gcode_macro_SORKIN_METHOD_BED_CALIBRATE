d5544640164fb86c4ee09fdf91ef8952ea9f554c
art1234567891 <106118176+artx123@users.noreply.github.com>
2025-12-06 04:38:40 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+source /opt/config/mod/.shell/0.sh
+
+INC="[include plugins/gcode_macro-SORKIN_METHOD_BED_CALIBRATE/new_macro_bed_call.cfg]"
+FILE="/opt/config/mod_data/plugins.cfg"
+
+grep -q "${INC}" "${FILE}" || echo "${INC}" >> "${FILE}"
+
+echo "plugin installed"
+echo "REBOOT" >/tmp/printer
---
+++ uninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+source /opt/config/mod/.shell/0.sh
+
+INC="[include plugins/gcode_macro-SORKIN_METHOD_BED_CALIBRATE/new_macro_bed_call.cfg]"
+FILE="/opt/config/mod_data/plugins.cfg"
+
+if grep -q "${INC}" "${FILE}"; then
+    
+    sed -i "/${INC}/d" "${FILE}"
+    echo "Строка удалена из ${FILE}"
+else
+    echo "Строка не найдена в ${FILE}"
+fi
+
+echo "plugin uninstalled"
+echo "REBOOT" >/tmp/printer
